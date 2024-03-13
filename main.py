aecd8fdad5d77e6824715ee9204cb36699c72998
orbitstabilizer <71184281+orbitstabilizer@users.noreply.github.com>
2024-03-12 20:33:24 -0400

    Add demo 2

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import threading
 
 def demo1():
     symbols = BinanceAPI.get_trading_pairs()
-    filter = list(symbols.keys())[:10]
+    # filter = list(symbols.keys())[:10]
+    filter = ["btcusdt", "ethusdt", "usdttry", "btctry", "ethbtc", "btceur"]
     filtered_symbols = {k: symbols[k] for k in filter}
 
-    # symbols = ["btcusdt", "ethusdt", "usdttry", "btctry", "ethbtc", "btceur"]
 
     ws = BinanceAPI(symbols=filtered_symbols)
     t = threading.Thread(target=ws.run_forever)
@@ -16,18 +16,18 @@ def demo1():
 
     cnt = 0
     while cnt < 1000:
-        print(f'{"":<5}', end=" ")
+        print(f'{"":<10}', end=" ")
         for u in ws.adj:
-            print(f'{u:<5}', end=" ")
+            print(f'{u:<10}', end=" ")
         print()
 
         for v in ws.adj:
-            print(f'{v:<5}', end=" ")
+            print(f'{v:<10}', end=" ")
             for u in ws.adj:
                 if v in ws.adj[u]:
-                    print(f'{ws.adj[u][v]:<5.2f}', end=" ")
+                    print(f'{ws.adj[v][u]:<10.6f}', end=" ")
                 else:
-                    print(f'{"":<5}', end=" ")
+                    print(f'{" ":<10}', end=" ")
             print()
         time.sleep(1)
         cnt += 1
@@ -38,5 +38,36 @@ def demo1():
 
     print("Done!")
 
+def demo2():
+    symbols = BinanceAPI.get_trading_pairs()
+    filter = list(symbols.keys())[:150]
+    # filter = ["btcusdt", "ethusdt", "usdttry", "btctry", "ethbtc", "btceur"]
+    filtered_symbols = {k: symbols[k] for k in filter}
+    N = sum([len(v) for v in filtered_symbols.values()])
+    print(N)
+    assert N <= 500 , f"Too many symbols: {N} > 500"
+
+
+    ws = BinanceAPI(symbols=filtered_symbols)
+    t = threading.Thread(target=ws.run_forever)
+
+    t.start()
+
+    while True:
+        time.sleep(0.1)
+        # print(ws.adj)
+        it = list(ws.arbitrage.items())
+        it.sort(key=lambda x: x[1])
+        if len(it) > 0:
+            print(*it, sep="\n")
+            print('--'*10)
+
+    ws.close()
+
+    t.join()
+
+    print("Done!")
+    
+
 if __name__ == "__main__":
-    demo1()
+    demo2()
